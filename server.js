b62d90b65d23e0264c13a6d2aa42af9d50d04824
praveen nigam <praveennigam1999@gmail.com>
2024-06-03 01:28:03 +0530

    updated
    last one

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = dirname(__filename);
+
 
 //connect frontend to backend
 
