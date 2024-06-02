6cf8186e51d536e57bc1981365ccfbec95fff414
praveen nigam <praveennigam1999@gmail.com>
2024-06-03 01:52:13 +0530

    just
    now

--- server.js
+++ server.js
@@ -1,69 +1,55 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
+
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
+import 'dotenv/config';
 
 const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = dirname(__filename);
 
-
-//connect frontend to backend
-
-
-import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
-import userRouter from "./routes/userRoute.js";
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
-// app config database
+// App config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-
-// db connection
+// DB connection
 connectDB();
 
-//static files
-app.use(express.static(path.join(__dirname, "./admin/dist")));
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
+// Static files
+app.use(express.static(join(__dirname, './admin/dist')));
+app.use(express.static(join(__dirname, './frontend/dist')));
 
-
-
-app.get("*", function(req, res) {
-
-    res.sendFile(path.join(__dirname, "./admin/dist/index.html"));
-})
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
-})
-
-
-
-
-
-
-
-
-// api endpoint
-app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'))
-app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
-
-
-
-
-app.get("/", (req, res) => {
-    res.send("API Working");
+// Routes
+app.get('*', function(req, res) {
+    res.sendFile(join(__dirname, './admin/dist/index.html'));
 });
 
+app.get('/', function(req, res) {
+    res.sendFile(join(__dirname, './frontend/dist/index.html'));
+});
+
+// API endpoints
+app.use('/api/food', foodRouter);
+app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+
+app.get('/', (req, res) => {
+    res.send('API Working');
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
