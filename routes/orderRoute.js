84e2acad949106049676b4f0195662ba8253235e
praveen nigam <praveennigam1999@gmail.com>
2024-06-03 03:29:13 +0530

    fbdsfg
    dg

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,17 +1,13 @@
 import express from 'express';
-import authMiddleware from "../middleware/auth.js"
 import { placeOrder, verifyOrder, userOrders, listOrders, updateStatus } from '../controllers/orderController.js';
-
-
+import { authenticateToken, authorizeAdmin } from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
-
-orderRouter.post('/place', authMiddleware, placeOrder);
+orderRouter.post('/place', authenticateToken, placeOrder);
 orderRouter.post('/verify', verifyOrder);
-orderRouter.post('/userOrders', authMiddleware, userOrders);
-orderRouter.get('/list', listOrders);
-orderRouter.post('/status', updateStatus);
-
+orderRouter.post('/userOrders', authenticateToken, userOrders);
+orderRouter.get('/list', authenticateToken, authorizeAdmin, listOrders);
+orderRouter.post('/status', authenticateToken, authorizeAdmin, updateStatus);
 
 export default orderRouter;
