50ecb4881d3127d2426eada93c0c911b1af9effd
praveen nigam <praveennigam1999@gmail.com>
2024-06-03 03:48:31 +0530

    sdgr
    rtg

--- config/db.js
+++ config/db.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true // This line causes the error, so remove it
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Database connected successfully");
     } catch (error) {
         console.error("Error connecting to database:", error);
